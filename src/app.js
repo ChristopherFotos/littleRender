66063f951de48655192ab0e3813e3c85d4dba5d4
chris fotos <chrisfotos.developer@gmail.com>
2022-10-13 00:30:26 -0400

    more clean up

--- src/app.js
+++ src/app.js
@@ -2,14 +2,10 @@ import './style.scss';
 import { html, render } from 'lit-html';
 import makeStore from './makeStore';
 
-const root = (data) => html`<h1>${data.text}</h1>
-	${btn(data.text)} ${comp3(data.text)} `;
+const root = (data) => html`<h1>${data.text}</h1>`;
 
 const data = {
 	text: 'HELLO',
-	booty: 'NONONO',
-	list: [],
-	width: 10,
 };
 
 export const store = makeStore(root, document.body, data);
