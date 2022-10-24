9cf286158bd596cbc5faab972be0f15006182f52
chris fotos <chrisfotos.developer@gmail.com>
2022-10-24 01:20:11 -0400

    add local state functionality

--- src/littleRender.js
+++ src/littleRender.js
@@ -1,8 +1,8 @@
 import { render } from 'lit-html';
 import { Directive, directive } from 'lit-html/directive.js';
 import { noChange } from 'lit';
-import copyObject from 'lodash/_copyObject';
-import copyArray from 'lodash/_copyArray';
+import cloneDeep from 'lodash/cloneDeep';
+import { uniqueId } from 'lodash';
 
 /* add a hooks array to each value, and then use a getter 
 trap to return the value when the property is accessed. 
@@ -10,10 +10,8 @@ in the setter trap, execute every state change hook. maybe also add
 access hooks.*/
 
 export const makeStore = (root, rootElement, data) => {
-	const store = {
-		...data,
-		root,
-	};
+	const store = cloneDeep(data);
+	store.root = root;
 
 	const handler = {
 		set(target, prop, value) {
@@ -21,6 +19,15 @@ export const makeStore = (root, rootElement, data) => {
 				target.root = value;
 				return true;
 			}
+
+			if (prop.startsWith('NO_RENDER') && !target.hasOwnProperty(prop)) {
+				console.log('preventing re-render');
+				target[prop] = value;
+				return true;
+			}
+
+			console.log('re-rendering proxy', prop);
+
 			target[prop] = value;
 			render(target.root(target), rootElement);
 			return true;
@@ -34,30 +41,26 @@ export const makeComponent = (component) => {
 	class ComponentDirective extends Directive {
 		constructor(partInfo) {
 			super(partInfo);
-
-			// Run the component function to do the initial setup, call hooks etc.
-			component();
-
 			this.part = partInfo;
 			this.component = component;
 			this.dependencies = [];
+			this.localState = [];
+			this.localStatePointer = 0;
 
 			// At the start of a components lifecycle, the current property
 			// will be a function that calls the component's initializer, if
 			// it has one. After calling the initializer and returning a
 			// renderable  value, it will set this.current to this.subsequent
-			this.current = (data, { _self, initialValues }) => {
+			this.current = (data) => {
 				let initValues;
+
 				if (this.component.initializer) {
-					initValues = this.component.initializer(data, _self);
+					initValues = this.component.initializer();
 				}
 
 				this.initValues = initValues;
 
-				const rendered = this.component(data, {
-					initialValues: initValues,
-					_self: _self,
-				});
+				const rendered = this.component(data, this.generateApi(this));
 
 				this.current = this.subsequent;
 
@@ -66,18 +69,73 @@ export const makeComponent = (component) => {
 
 			// the function that runs on all renders after the first one.
 			// just returns the component's rendered markup.
-			this.subsequent = (data, { initialValues, _self }) => {
-				return this.component(data, {
-					initialValues: initialValues,
-					_self: _self,
-				});
+			this.subsequent = (data) => {
+				return this.component(data, this.generateApi(this));
 			};
 			this.renders = 0;
+
+			// Run the component function to do the initial setup, call hooks etc.
+			// console.log(this.generateApi());
+			// component(this.generateApi);
 		}
 
-		addState() {}
+		addState = (store, state) => {
+			console.log('addstate');
+			// debugger;
+			if (this.renders === 0) {
+				const key = `NO_RENDER-${uniqueId()}`;
 
-		addDependency(store, key) {
+				store[key] = state;
+				this.addDependency(store, key);
+
+				const returnVal = [
+					store[key],
+
+					(value) => {
+						console.log('PARAM: ', value);
+						// this.localState[this.localStatePointer][0] = value;
+						// console.log('TARGET: ', this.localState[this.localStatePointer][0]);
+						store[key] = value;
+						console.log('val ', store[key]);
+						return store[key];
+					},
+				];
+
+				returnVal.push(key);
+
+				this.localState.push(returnVal);
+
+				return returnVal;
+			} else if (
+				this.renders > 0 &&
+				this.localStatePointer <= this.localState.length - 1
+			) {
+				const returnVal = this.localState[this.localStatePointer];
+
+				// Manually syncing the localState with the store. this is BAD,
+				// we need a single source of trueth that we can subscribe to
+				returnVal[0] = store[returnVal[2]];
+
+				this.localStatePointer += 1;
+				return returnVal;
+			} else {
+				this.localStatePointer = 0;
+				const returnVal = this.localState[this.localStatePointer];
+
+				// Manually syncing the localState with the store. this is BAD,
+				// we need a single source of trueth that we can subscribe to
+				returnVal[0] = store[returnVal[2]];
+
+				this.localStatePointer += 1;
+				return returnVal;
+			}
+		};
+
+		addInit = (initializer) => {
+			this.component.initializer = initializer;
+		};
+
+		addDependency = (store, key) => {
 			// Holds the current value of the dependency being added. Will
 			// be compared to the current value on subsequent renders to
 			// determine whether the component should re-render or not.
@@ -86,13 +144,13 @@ export const makeComponent = (component) => {
 			// If the dependency is an object or an array, store a copy of
 			// it in lastKNownValue. If it's a primitive, just store the value.
 			if (Array.isArray(store[key])) {
-				lastKnownValue = copyArray(store[key]);
+				lastKnownValue = cloneDeep(store[key]);
 			} else if (
 				typeof store[key] === 'object' &&
 				!Array.isArray(store[key]) &&
 				store[key] !== null
 			) {
-				lastKnownValue = copyObject(store[key]);
+				lastKnownValue = cloneDeep(store[key]);
 			} else {
 				lastKnownValue = store[key];
 			}
@@ -106,7 +164,7 @@ export const makeComponent = (component) => {
 			};
 
 			this.dependencies.push(dependency);
-		}
+		};
 
 		shouldUpdate() {
 			// Caching the array length for performance.
@@ -139,25 +197,31 @@ export const makeComponent = (component) => {
 			return false;
 		}
 
+		generateApi(self) {
+			return {
+				initialValues: self.initValues,
+				addState: self.addState,
+				addInit: self.addInit,
+				addDependency: self.addDependency,
+				_self: self,
+			};
+		}
+
 		render(data) {
+			if (this.renders < 1) {
+				this.component(data, this.generateApi(this));
+			}
 			this.renders++;
 
 			// Check if the component needs to update
 			let willUpdate = this.shouldUpdate();
+			console.log('SHOULD UPDATE? ', willUpdate);
 
 			if (this.renders > 1 && !willUpdate) return noChange;
 			if (!this.component) this.component = component;
 
 			// Return renderable markup
-			return this.current(data, {
-				/*
-				This object can be used as the API for this object. Feed methods and properties
-			  from this class into this object, and the user can destructure whichever pieces 
-				they want. 
-				*/
-				initialValues: this.initValues,
-				_self: this,
-			});
+			return this.current(data);
 		}
 	}
 
--- src/root.js
+++ src/root.js
@@ -1,29 +1,41 @@
 import { html } from 'lit';
 import { makeComponent } from './littleRender';
 import { store } from './app.js';
-import { v4 as uuid } from 'uuid';
 
-const numRaw = (data) => {
-	console.log('rendering number');
-	return html`<h1>${store.number}</h1>`;
+const numRaw = (data, { addState, addInit, addDependency, _self }) => {
+	// the newly set state isn't reflecting here
+	const [state, setState, s1id] = addState(store, 1);
+	const [state2, setState2, s2id] = addState(store, 2);
+	const [state3, setState3, s3id] = addState(store, 3);
+	const [state4, setState4, s4id] = addState(store, 4);
+
+	// which means the next time i go and setState, it's setting it to
+	// the same value as before, which means that 'noChange' is returned,
+	// and this component does not re-render
+
+	console.log('RENDER');
+	const click = () => {
+		store[s2id] = store[s2id] + 1;
+	};
+
+	addInit(() => {
+		addDependency(store, 'number');
+	});
+
+	return html`
+		<h1>${state2}</h1>
+		<button @click=${click}>Here click</button>
+	`;
 };
 
-numRaw.initializer = (_data, instance) => {
-	instance.addDependency(store, 'number');
-	console.log('NUM init', instance);
-};
+const timer = (data, { addState, addInit, addDependency }) => {
+	addInit((_data, instance) => {
+		addDependency(store, 'text');
+	});
 
-export const num = makeComponent(numRaw);
-console.log('NUM RETURN VAL:', num);
-
-const timer = (data) => {
 	console.log('rendering text');
 	return html`<h1>${store.text}</h1>`;
 };
 
-timer.initializer = (_data, instance) => {
-	instance.addDependency(store, 'text');
-	console.log('TEXT init', instance);
-};
-
+export const num = makeComponent(numRaw);
 export default makeComponent(timer);
